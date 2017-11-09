66c691dee56779acfe48cf2d393966fe240237b7
Amir Ali Jiwani <amirali.jiwani89@gmail.com>
2017-11-09 21:43:36 +0800

    Adds test coverage configuration

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ var config = {
   moduleFileExtensions: ['js', 'jsx'],
   moduleDirectories: ['node_modules', 'src'],
   setupFiles: [path.resolve(__dirname, 'helpers', 'shim.js')],
+  coveragePathIgnorePatterns: ["/helpers/", "/node_modules/"]
 };
 
 module.exports = config;
